be943dbdb3ae2c892ddf799c590ff017ade81e13
Nensin <pereznensin@gmail.com>
2024-12-02 17:24:27 -0400

    Cambios en los meta de cada pagina, Se creo la pagina de conta us con su fomulario y la pagina de user tambien un js para user junto con CSS y JS para las paginas creadas

---
+++ JS/user.js
@@ -0,0 +1,12 @@
+
+         
+            const form = document.getElementById('loginForm');
+        
+            
+            form.addEventListener('submit', function(event) {
+                event.preventDefault(); 
+        
+                
+                window.location.href = 'index.html';
+            });
+      
