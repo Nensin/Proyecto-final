f64b2a06d0106a9ee32369251740ca048c99a5ed
Nensin <pereznensin@gmail.com>
2024-12-02 21:47:34 -0400

    Comente todos los html y los JS

--- JS/user.js
+++ JS/user.js
@@ -1,11 +1,11 @@
 function redirectToPreviousPage() {
-    
+    // Comprueba si el documento tiene una página de referencia (referer)
     if (document.referrer) {
-       
+        // Si existe, redirige al usuario a esa página de referencia
         window.location.href = document.referrer;
     } else {
-       
+        // Si no hay página de referencia, redirige a la página predeterminada
         window.location.href = 'index.html';  
     }
-    return false; 
+    return false; // Previene cualquier acción predeterminada que invoque esta función
 }
